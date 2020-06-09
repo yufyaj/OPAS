b4e3647fda50c60f250ca27ade064a4c41244a0e
yufyaj <61311691+yufyaj@users.noreply.github.com>
2020-06-09 17:45:29 +0900

    Delete log.py

--- log.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-# ƒƒOo—Í
-def OutLog(output):
-    print(Now() + " " + output)
-
-# Œ»İ‚Ì‚ğæ“¾
-def Now():
-    return datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
